2c07e27c4ae4d7a7db6efa5876a5c7d3024bcc15
daniel ouattara <daniel.ouattara@gmx.com>
2022-06-09 18:39:58 +0200

    TypeScript starting Home

---
+++ 01_TypeScript_Home/index.ts
@@ -0,0 +1,26 @@
+/*------------------------------------------------------
+Daniel OUATTARA- daniel.ouattara_AT_gmx.com
+09 06 2022
+TypeScript Tutorial : https://www.w3schools.com/typescript/ 
+
+------------------------------------------------------*/
+
+/* TypeScript Tutorial
+=======================
+
+TypeScript is JavaScript with added syntax for types.
+
+Examples in Each Chapter
+
+
+You can edit TypeScript code and view the result in your browser.
+
+Example */
+
+console.log("Hello World!");
+
+/*
+Click on the "Try it Yourself" button to see how it works.
+
+We recommend reading this tutorial in the sequence listed in the left menu.
+TypeScript Exercises */
