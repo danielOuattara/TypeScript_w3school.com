fefe3b98b728409b9050f8519685155be80828e6
danielOuattara <daniel.ouattara@gmx.com>
2023-01-30 16:01:36 +0100

    intro

--- 02_TypeScript_Introduction/index.ts
+++ 02_TypeScript_Introduction/index.ts
@@ -10,6 +10,7 @@ TypeScript Tutorial : https://www.w3schools.com/typescript/
 
 TypeScript is JavaScript with added syntax for types.
 
+
 What is TypeScript ?
 --------------------
 TypeScript is a syntactic superset of JavaScript which 
@@ -19,6 +20,7 @@ add types.
 
 TypeScript being a "Syntactic Superset" means that it shares 
 the same base syntax as JavaScript, but adds something to it.
+
 
 Why should I use TypeScript ?
 ------------------------------
@@ -35,7 +37,7 @@ around within the code, and has the ability to report errors
 when the types don't match.
 
 For example, TypeScript will report an error when passing a 
-string into a function that expects a number. JavaScript will not.
+string into a function that expects a number. JavaScript won't.
 
 Note: TypeScript uses compile time type checking. Which means it 
       checks if the specified types match before running the code, 
@@ -51,5 +53,4 @@ The next section shows how to get the compiler setup for a local
 project.
 
 Some popular code editors, such as Visual Studio Code, have 
-built-in TypeScript support and can show errors as you write code!
-TypeScript Exercises */
+built-in TypeScript support and can show errors as you write code! */
