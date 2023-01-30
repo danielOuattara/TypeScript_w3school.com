a547e3f19290a04187525f9d2910f486cf0d75ba
danielOuattara <daniel.ouattara@gmx.com>
2023-01-30 22:31:38 +0100

    aliases & interfaces

---
+++ 10_Typescript_Aliases_Interfaces/index.js
@@ -0,0 +1,23 @@
+/*------------------------------------------------------
+Daniel OUATTARA- daniel.ouattara_AT_gmx.com
+09 06 2022
+TypeScript Tutorial : https://www.w3schools.com/typescript/
+
+------------------------------------------------------*/
+var carYear = 2001;
+var carType = "Toyota";
+var carModel = "Corolla";
+var car = {
+    year: carYear,
+    type: carType,
+    model: carModel
+};
+var rectangle_1 = {
+    height: 20,
+    width: 10
+};
+var coloredRectangle = {
+    height: 20,
+    width: 10,
+    color: "red"
+};
---
+++ 10_Typescript_Aliases_Interfaces/index.ts
@@ -0,0 +1,77 @@
+/*------------------------------------------------------
+Daniel OUATTARA- daniel.ouattara_AT_gmx.com
+09 06 2022
+TypeScript Tutorial : https://www.w3schools.com/typescript/ 
+
+------------------------------------------------------*/
+
+/* TypeScript Type Aliases and Interfaces
+==========================================
+
+TypeScript allows types to be defined separately from 
+the variables that use them.
+
+Aliases and Interfaces allows types to be easily shared 
+between different variables/objects.
+
+
+Type Aliases
+-------------
+Type Aliases allow defining types with a custom name (an Alias).
+Type Aliases can be used for primitives like string or more 
+complex types such as objects and arrays */
+
+type CarYear = number;
+type CarType = string;
+type CarModel = string;
+
+type Car = {
+  year: CarYear;
+  type: CarType;
+  model: CarModel;
+};
+
+const carYear: CarYear = 2001;
+const carType: CarType = "Toyota";
+const carModel: CarModel = "Corolla";
+
+const car: Car = {
+  year: carYear,
+  type: carType,
+  model: carModel,
+};
+
+/*
+Interfaces
+-----------
+Interfaces are similar to type aliases, except they only apply 
+to object types. */
+
+interface Rectangle {
+  height: number;
+  width: number;
+}
+
+const rectangle_1: Rectangle = {
+  height: 20,
+  width: 10,
+};
+
+/* 
+Extending Interfaces
+---------------------
+
+Interfaces can extend each other's definition.
+
+Extending an interface means you are creating a new interface 
+with the same properties as the original, plus something new. */
+
+interface ColoredRectangle extends Rectangle {
+  color: string;
+}
+
+const coloredRectangle: ColoredRectangle = {
+  height: 20,
+  width: 10,
+  color: "red",
+};
